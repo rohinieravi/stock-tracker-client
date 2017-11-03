c74c4d32a87801fddefa2947b966b0534cc47c02
rohinieravi <rohinieravi@yahoo.com>
2017-11-02 19:14:30 -0700

    added bug fix

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import {createStore,applyMiddleware, combineReducers, compose} from 'redux';
+import {createStore,applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {reducer as formReducer} from 'redux-form';
 import {stockReducer} from './reducers';
